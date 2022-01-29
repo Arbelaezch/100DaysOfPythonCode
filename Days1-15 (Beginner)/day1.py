c8d7e96811d8df8879623f20cdbcdfa568f3e154
Arbelaezch <arbelaezch@gmail.com>
2022-01-28 18:51:17 -0700

    added day 1 program

---
+++ Days1-15 (Beginner)/day1.py
@@ -0,0 +1,6 @@
+# Simple band name generator program.
+# For input() and basic string manipulation.
+print("Welcome to the Band Name Generator.")
+city = input('What\'s the name of the city you grew up in?\n')
+pet = input("What's a good name for a pet?\n")
+print("Your band could be " + city + " " + pet)
