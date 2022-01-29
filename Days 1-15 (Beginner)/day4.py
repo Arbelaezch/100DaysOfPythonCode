59943a9361d8163d1dc3d1e342b52609dfeca6c7
Arbelaezch <arbelaezch@gmail.com>
2022-01-29 00:13:08 -0700

    added day 4

---
+++ Days 1-15 (Beginner)/day4.py
@@ -0,0 +1,57 @@
+# Rock Paper Scissors
+
+import random
+
+
+
+
+print("Welcome to Rock, Paper, Scissors!")
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+random_int = random.randint(0,2)
+
+hand = ["""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+""", """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+""", """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""]
+
+row1 = ['Tie', 'You Win', 'You Lose']
+row2 = ['You Lose', 'Tie', 'You Win']
+row3 = ['You Win', 'You Lose', 'Tie']
+rps = [row1, row2, row3]
+
+
+print(hand[choice])
+print(f"Computer chose: {random_int}")
+print(hand[random_int])
+
+print(rps[random_int][choice])
+print("-")
+
+
+
+
+
+
+
+
+
+
+
