69eed35c584e334e04f7e181819950559c8a8d23
Arbelaezch <arbelaezch@gmail.com>
2022-01-31 05:57:06 -0700

    DRY day8

--- Days1-15:Beginner/day8.py
+++ Days1-15:Beginner/day8.py
@@ -22,56 +22,44 @@ run = "yes"
 
 art()
 
-def encode1(alphabet):
+def caesar(alphabet, type):
 	message = input('Type your message:\n')
 	message_list = list(message)
 
 	shift = int(input("Type the shift number:\n"))
 	
 	for i in range(0, len(message_list)):
-		letter = str(message_list[i])
-		position = int(alphabet.find(letter))
-		new_position = position + shift
-		if new_position > 25:
-			new_position -= 26
-		message_list[i] = alphabet[new_position]
+		if message_list[i] in alphabet_list:
+			letter = str(message_list[i])
+			position = int(alphabet.find(letter))
+			if type == "encode":
+				new_position = position + shift
+				if new_position > 25:
+					new_position -= 26
+			else:
+				new_position = position - shift
+				if new_position < 0:
+					new_position += 26
+			message_list[i] = alphabet[new_position]
 
 	encoded = "".join(message_list)
 	return encoded
 
 
-def decode1(alphabet):
-	message = input('Type your message:\n')
-	message_list = list(message)
-
-	shift = int(input("Type the shift number:\n"))
-	
-	for i in range(0, len(message_list)):
-		letter = str(message_list[i])
-		position = int(alphabet.find(letter))
-		new_position = position - shift
-		if new_position < 0:
-			new_position += 26
-		message_list[i] = alphabet[new_position]
-
-	decoded = "".join(message_list)
-	return decoded
-
-
-
 
 
 while run == "yes":
-	encode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+	type = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-	if encode == "encode":
-		encoded_result = encode1(alphabet)
-		print(f"Here's the encoded result: {encoded_result}")
+	if type == "encode" or type == "decode":
+		result = caesar(alphabet, type)
+		print(f"Here's the encoded result: {result}")
+		run = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n")
 	else:
-		decoded_result = decode1(alphabet)
-		print(f"Here's the decoded result: {decoded_result}")
+		print("Try again.")
 
-	run = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n")
+
+
 
 
 
