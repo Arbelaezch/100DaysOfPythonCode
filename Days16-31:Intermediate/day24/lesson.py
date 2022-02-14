af6da6d8d4700d772be41590af6c34121df37df3
Arbelaezch <arbelaezch@gmail.com>
2022-02-14 16:36:05 -0700

    day 24 - mail merge

---
+++ Days16-31:Intermediate/day24/lesson.py
@@ -0,0 +1,19 @@
+# # Open and close a file to read/write/append its content
+# file = open("Days16-31:Intermediate/day24/my_file.txt")
+# content = file.read()
+# print(content)
+
+# # Can open a file under pseudonym and for a specific set of actions. Closes automatically.
+# with open("Days16-31:Intermediate/day24/my_file.txt", mode="a") as file:
+# 	file.write("\ntest")
+# 	print(file)
+
+
+# Working Directory: The directory the program is located in.
+# Absolute Path: The path to a file from the root directory.
+# 		- Paths begin with /
+# Relative Path: The path to a file from the working directory.
+# 		- Path does not begin with /
+
+
+
--- Days16-31:Intermediate/day24/main.py
+++ Days16-31:Intermediate/day24/main.py
@@ -1,12 +1,23 @@
-# Open and close a file to read/write/append its content
-file = open("Days16-31:Intermediate/day24/my_file.txt")
-content = file.read()
-print(content)
+# Mail Merge Project
+# Takes names from one file, then inserts each name into an email, and
+# saves each version of the email.
 
-# Can open a file under pseudonym and for a specific set of actions. Closes automatically.
-with open("Days16-31:Intermediate/day24/my_file.txt", mode="a") as file:
-	file.write("\ntest")
-	print(file)
+with open("Days16-31:Intermediate/day24/Input/names/invited_names.txt") as name_file:
+	names = name_file.readlines()
+
+	i = 0
+	for i in range(0, len(names)):
+		names[i] = names[i].strip("\n")
+
+
+with open("Days16-31:Intermediate/day24/Input/Letters/starting_letter.txt") as letter_file:
+	letter = letter_file.read()
+
+	for name in names:
+		new_letter = letter.replace("[name]", name)
+		f = open("Days16-31:Intermediate/day24/Output/ReadyToSend/"+ name + ".txt", "w")
+		f.write(new_letter)
+		f.close()
 
 
 
