9dd27a830298937728e99bb57281680a80942bb9
Arbelaezch <arbelaezch@gmail.com>
2022-05-10 15:07:38 -0600

    Finish day75 plotly files

--- Days59-80:Advanced/day75/main.py
+++ Days59-80:Advanced/day75/main.py
@@ -1,4 +1,7 @@
+# Data analyzes the Android App store for various data.
+
 import pandas as pd
+import plotly.express as px
 
 
 # Show numeric output in decimal format e.g., 2.15
@@ -6,3 +9,12 @@ pd.options.display.float_format = '{:,.2f}'.format
 
 
 df_apps = pd.read_csv('Days59-80:Advanced/day75/apps.csv')
+
+# Drop unused columns and remove NaN values
+df_apps_clean = df_apps.dropna()
+
+# Find and remove duplicates
+duplicated_rows = df_apps_clean[df_apps_clean.duplicated()]
+df_apps_clean = df_apps_clean.drop_duplicates(subset=['App', 'Type', 'Price'])
+df_apps_clean[df_apps_clean.App == 'Instagram']
+print(df_apps_clean.shape)
